a249f2e946a1b9398a4908862dc12ba3f227b999
Gleb <42696774+t0rrex@users.noreply.github.com>
2021-06-06 17:44:06 +0300

    getPhotos

--- src/tasks/getPhotos/index.js
+++ src/tasks/getPhotos/index.js
@@ -1,10 +1,11 @@
-import "../../styles.css";
+import '../../styles.css';
 
-const title = document.createElement("h1");
-title.innerHTML = "Get Photos from API and show them!";
+const title = document.createElement('h1');
+title.innerHTML = 'Get Photos from API and show them!';
 
-document.getElementById("app").appendChild(title);
+document.getElementById('app').appendChild(title);
 
+// Current solution - for JS native option
 // Получить картинки через классовый и
 // функциональный компоненты реакт, а также нативно
 // вывести в виде круглых кликабельных изображений,
@@ -13,6 +14,20 @@ document.getElementById("app").appendChild(title);
 // Адрес получения картинок - https://picurl.herokuapp.com/users/
 // id картинок: 2, 7, 1, 8, 3, 9
 
-//get photos request handler
+// SOLUTION:
+
+const getImages = async (url, ids) => {
+  return !Array.isArray(ids)
+    ? fetch(url + ids)
+    : Promise.all(ids.map(id => fetch(url + id).then(res => res.json())));
+};
+
+getImages('https://picurl.herokuapp.com/users/', 2)
+  .then(res => res.json())
+  .then(result => console.log('single result', result))
+  .catch(() => console.log('hui tebe a ne result'));
+
+getImages('https://picurl.herokuapp.com/users/', [2, 7, 1, 8, 3, 9])
+  .then(result => console.log('multi results', result));
 
 //insert photos in html
