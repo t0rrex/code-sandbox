1b8bc37363eb19e91948ae14815b01581abe6ede
Gleb <42696774+t0rrex@users.noreply.github.com>
2021-06-06 12:45:40 +0300

    Initial commit

---
+++ src/array/arrayMethods.js
@@ -0,0 +1,18 @@
+const sort = (arr) => {
+  let temp;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      temp = arr[j];
+      if (arr[j] > arr[j + 1]) {
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+};
+
+const sorted = sort([1, 2, 4, 5, -3, 1, 4, 7, 8, -5]);
+console.log("sorted", sorted);
