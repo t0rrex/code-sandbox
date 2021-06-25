327fa1ad8d9e3e0dd22829c2648164b3d3faefab
Gleb <42696774+t0rrex@users.noreply.github.com>
2021-06-25 21:26:01 +0300

    code splitted by directories

--- src/JS/array/arrayMethods.js
+++ src/JS/array/arrayMethods.js
@@ -1,12 +1,12 @@
-import "../../styles.css";
+import '../../styles.css';
 
-document.getElementById("app").innerHTML = `
+document.getElementById('app').innerHTML = `
 <h1>Array Methods!</h1>
 <div>Create our own implementation of built-in array methods</div>
 `;
 
 //sort
-const sort = (arr) => {
+const sort = arr => {
   let temp;
 
   for (let i = 0; i < arr.length; i++) {
@@ -23,4 +23,4 @@ const sort = (arr) => {
 };
 
 const sorted = sort([1, 2, 4, 5, -3, 1, 4, 7, 8, -5]);
-console.log("sorted", sorted);
+console.log('sorted', sorted);
